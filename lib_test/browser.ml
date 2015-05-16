aaaa106ae5cf698eaf50ec5099c1393c55ea5281
Vincent Bernardoff <vb@luminar.eu.org>
2015-05-16 11:10:49 +0200

    Added a browser program to dump .scid files in human-readable format.

---
+++ lib_test/browser.ml
@@ -0,0 +1,15 @@
+open Scid
+
+let io_buffer_size = 4096
+
+let () =
+  let ic = open_in_bin Sys.argv.(1) in
+  let d = D.make @@ `Channel ic in
+  let rec print () =
+    match D.decode d with
+    | `R r -> Format.printf "%a@." R.pp r; print ()
+    | `End -> ()
+    | `Error `Header_invalid s -> Printf.eprintf "Invalid header %S. aborting.\n" s; exit 1
+    | `Error `Eof s -> Printf.eprintf "Premature EOF: %S not parsed.\n" s; exit 1
+    | `Await -> assert false
+  in print ()
--- pkg/build.ml
+++ pkg/build.ml
@@ -7,6 +7,7 @@ let () =
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "lib/scid";
     Pkg.bin ~auto:true "lib_test/suite";
+    Pkg.bin ~auto:true "lib_test/browser";
     Pkg.bin ~auto:true "lib_test/copy";
     Pkg.bin ~auto:true "lib_test/test_decoder";
   ]
